836c7945e079e470842028d8818292cd1f1fbafa
Timas Jackus <jackustimas@gmail.com>
2020-03-22 11:36:08 +0200

    update enum

--- Graph/src/entities/enums/PersonalityTraitEnum.ts
+++ Graph/src/entities/enums/PersonalityTraitEnum.ts
@@ -1,8 +1,14 @@
 import { registerEnumType } from 'type-graphql';
 
 export enum PersonalityTraitEnum {
-    BookLover = 0,
-    Traveller = 1
+    Fitness = 0,
+    Cooking = 1,
+    Travel = 2,
+    Books = 3,
+    Music = 4,
+    Games = 5,
+    Pets = 6,
+    Kids = 7
 }
 
 registerEnumType(PersonalityTraitEnum, {
